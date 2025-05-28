54c24979c61ed18ed627dc41149e7afafa279dbc
Ekaterina Ivanova <katenika158@gmail.com>
2025-05-29 00:36:17 +0300

    2: Add input file validation

--- cmd/anonymize.go
+++ cmd/anonymize.go
@@ -5,7 +5,9 @@ import (
 	"github.com/spf13/cobra"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"unicode/utf8"
 )
 
 type Config struct {
@@ -16,15 +18,77 @@ type Config struct {
 	KeepComments   bool
 }
 
-func DoesFileExist(path string) (found bool, err error) {
+func FileExists(path string) (exists bool, err error) {
 	if _, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
-			err = nil
+			return false, nil
 		}
-	} else {
-		found = true
+		return false, err
 	}
-	return
+	return true, nil
+}
+
+func IsGoFile(path string) bool {
+	ext := filepath.Ext(path)
+	return ext == ".go"
+}
+
+func IsBinaryFile(path string) (isGo bool, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	buf := make([]byte, 512)
+	if _, err = file.Read(buf); err != nil {
+		return false, err
+	}
+
+	return !utf8.Valid(buf), nil
+}
+
+func IsFileEmpty(path string) (isEmpty bool, err error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return fileInfo.Size() == 0, nil
+}
+
+func ValidateGoFile(path string) (err error) {
+	// Input file existence check
+	exists, err := FileExists(path)
+	if err != nil {
+		return fmt.Errorf("file check failed: %v", err)
+	}
+	if !exists {
+		return fmt.Errorf("file %s does not exist", path)
+	}
+
+	// Input file type check
+	if !IsGoFile(path) {
+		return fmt.Errorf("file %s is not a .go file", path)
+	}
+
+	// Empty file validation
+	if empty, err := IsFileEmpty(path); err != nil {
+		return fmt.Errorf("failed to check file size: %w", err)
+	} else if empty {
+		return fmt.Errorf("file is empty: %s", path)
+	}
+
+	// Check if binary
+	if isBinary, err := IsBinaryFile(path); err == nil && isBinary {
+		return fmt.Errorf("file appears to be binary: %s", path)
+	}
+
+	return nil
 }
 
 func ValidateLanguage(language string) (normalized string, err error) {
@@ -47,6 +111,12 @@ func Validate(config *Config) error {
 		log.Fatalf("Language error: %v", err)
 	}
 	config.NormalizedLang = NormalizedLang
+
+	err = ValidateGoFile(config.InputFile)
+	if err != nil {
+		log.Fatalf("Input file type error: %v", err)
+	}
+
 	return nil
 }
 
@@ -65,14 +135,6 @@ var AnonymizeCmd = &cobra.Command{
 			log.Fatalf("Configuration error: %v", err)
 		}
 		fmt.Println(cfg.NormalizedLang)
-
-		// Input file existence check
-		fileExists, err := DoesFileExist(cfg.InputFile)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("\nInput file", cfg.InputFile, "exists:", fileExists, "\nLang:", cfg.NormalizedLang)
-
 	},
 }
 
